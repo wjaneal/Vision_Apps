efbad18832b25ade0c23c0f99b50fb44b7fecce4
William Neal <william.j.a.neal@gmail.com>
2014-12-29 22:14:11 -0500

    Output equations of central axes of each camera

--- 2D_to_3D.py
+++ 2D_to_3D.py
@@ -1,5 +1,8 @@
 from Mathfunctions import *
 
+#Pi is always handy:
+PI = 4*atan(1)
+
 #Set up number of pixels of each webcam in the X and Y directions
 C1X = 640
 C1Y = 480
@@ -23,4 +26,15 @@ line_2_m_right = 90.0-theta2+alpha
 
 #Determine equations for each line at the edges of the camera's field of view
 
+m1x = cos(PI*theta1/180)
+m1y = sin(PI*theta1/180)
+m2x = cos(PI*theta2/180)
+m2y = sin(PI*theta2/180)
+P01 = [d1,0,0]
+P02 = [d2,0,0]
+m1 = [m1x,m1y,0]
+m2 = [m2x,m2y,0]
+l1 = Line_3D(P01,m1)
+l2 = Line_3D(P02,m2)
+print l1.P0, l1.m, l2.P0, l2.m
 
